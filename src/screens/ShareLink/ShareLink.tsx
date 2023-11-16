085818be45c611103c184c8832c6cfa4b8354886
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-11-16 11:37:25 +0530

    initial commit for basic structure of all screens

---
+++ src/screens/ShareLink/ShareLink.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {LinkPreview} from '@flyerhq/react-native-link-preview';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <LinkPreview
+        containerStyle={styles.previewContainer}
+        enableAnimation
+        text="https://pinterest.com"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  previewContainer: {
+    width: '80%',
+    backgroundColor: '#f7f7f8',
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+});
+
+export default App;
