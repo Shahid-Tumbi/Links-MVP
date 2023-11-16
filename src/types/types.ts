9e0f4de436484a5726e5a8198cdf20e89ff3ba57
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-11-16 11:43:47 +0530

    adding types for props

---
+++ src/types/types.ts
@@ -0,0 +1,24 @@
+export type Post = {
+    id: string;
+    content: string;
+    image?: string;
+    likes: number;
+    profile: User;
+  };
+  
+  export type User = {
+    id: string;
+    name: string;
+    position: string;
+    image?: string;
+    backimage?: string;
+    about?: string;
+    experience?: Comments[];
+  };
+  
+  export type Comments = {
+    id: string;
+    title: string;
+    commentusername: string;
+    commentimage?: string;
+  };
