50bb6fc0a67eb65317d455e88f64c4ba3ac0a382
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-11-22 11:23:10 +0530

    PostItem looks better now mercifully

--- src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Startup } from './Startup/Startup';
-export { default as Login } from './Login/Login'
-export { default as Signup } from './Signup/Signup'
-export { default as OtpScreen } from './OtpScreen/OtpScreen'
-export { default as EditProfile } from './EditProfile/EditProfile'
-export { default as ProfileScreen } from './ProfileScreen/ProfileScreen'
