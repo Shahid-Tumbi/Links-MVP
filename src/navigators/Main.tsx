810cbdc243de68ff9276a007755cd942ce68b9d8
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-11-23 12:54:41 +0530

    User Profile done

--- src/navigators/Main.tsx
+++ src/navigators/Main.tsx
@@ -22,6 +22,7 @@ import CommentItem from '@/components/CommentItem/CommentItem';
 import SearchBarComponent from '@/components/SearchBar/SearchBar';
 import CommentInput from '@/screens/ShareLink/test';
 import test2 from '@/screens/ShareLink/test2';
+import UserProfile2 from '@/screens/UserProfile/UserProfile2';
 
 // import HomeFeed from '@/screens/HomeFeed/HomeFeed';
 // import ShareLink from '@/screens/ShareLink/ShareLink';
@@ -56,7 +57,7 @@ const TabNavigator = () => {
   const size = 25
   return <Tab.Navigator screenOptions={{
     tabBarStyle:{backgroundColor:Colors.primary,borderTopColor:Colors.primary}}}>
-      <Tab.Screen name="HomeScreen" component={ShareLink} options={{headerShown: false, tabBarShowLabel:false,
+      <Tab.Screen name="HomeScreen" component={UserProfile2} options={{headerShown: false, tabBarShowLabel:false,
         tabBarIcon: ({ focused, size }) => {
            return <Octicons name={ 'home' } color={focused ? blueColor : color} size={size} />
           }}}   />
